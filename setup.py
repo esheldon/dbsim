98ef71d274c3da5084709199266c7533bd627de7
Erin Sheldon <erin.sheldon@gmail.com>
2018-08-01 08:38:18 -0400

    initial commits

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+import glob
+from distutils.core import setup
+
+scripts=glob('bin/*')
+scripts = [s for s in scripts if '~' not in s]
+
+
+setup(
+    name="deblendsim", 
+    version="0.1.0",
+    description="Run deblenders on simulations",
+    license = "GPL",
+    author="Erin Scott Sheldon",
+    author_email="erin.sheldon@gmail.com",
+    scripts=scripts,
+    packages=['deblendsim'],
+)
