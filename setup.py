4d9f5f1f2d7a52e26b5ed766ef757772fd02179d
Erin Sheldon <erin.sheldon@gmail.com>
2018-08-01 14:03:24 -0400

    rename dbsim

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-import glob
+from glob import glob
 from distutils.core import setup
 
 scripts=glob('bin/*')
@@ -7,12 +7,12 @@ scripts = [s for s in scripts if '~' not in s]
 
 
 setup(
-    name="deblendsim", 
+    name="dbsim", 
     version="0.1.0",
     description="Run deblenders on simulations",
     license = "GPL",
     author="Erin Scott Sheldon",
     author_email="erin.sheldon@gmail.com",
     scripts=scripts,
-    packages=['deblendsim'],
+    packages=['dbsim'],
 )
